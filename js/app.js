ecd9e3ca17a8681470ecb2be41dd604e58c9ede9
Andrew Hallberg <hallbergandrew@gmail.com>
2014-05-01 10:13:47 -0700

    edit, markdown, moment.js

--- js/app.js
+++ js/app.js
@@ -10,11 +10,43 @@ Blog.Router.map(function() {
   this.resource('contact');
   this.resource('photos');
 });
-
+//Translates model into object for template
 Blog.PostsRoute = Ember.Route.extend({
   model: function() {
     return posts;
   }
+});
+
+Blog.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+//Controlls action state and responds to events from template Controller
+Blog.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  actions: {
+      edit: function() {
+        this.set('isEditing', true);
+      },
+
+      doneEditing: function() {
+        this.set('isEditing', false);
+      }
+  }
+});
+
+//Helper helps handle ember formatting
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
 
@@ -33,4 +65,12 @@ var posts = [{
   date: new Date('12-12-2012'),
   excerpt: "This is the excerpt al;sdkjf;alsdkfja;sldkfja;sldk",
   body: "I this is the main bodyI this is the main bodyI this is the main bodyI this is the main bodyI this is the main bodyI this is the main bodyI this is the main bodyI this is the main bodyI this is the main body"
+},
+{
+  id: '3',
+  title: "The Parley Letter",
+  author: { name: "d2h" },
+  date: new Date('12-24-2012'),
+  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
+  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."
 }];
